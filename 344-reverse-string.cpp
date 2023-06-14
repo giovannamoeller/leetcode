589b01e7ef18e3b6f64c80ed01e7863248bc0e9b
Giovanna Moeller <giovannagmoeller@gmail.com>
2023-06-14 15:32:19 -0300

    reverse string

---
+++ 344-reverse-string.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void reverseString(vector<char>& s) {
+  int left = 0, right = (int)s.size() - 1;
+  
+  while (right > left) {
+    swap(s[right], s[left]);
+    right -= 1;
+    left += 1;
+  }
+}
+
+int main() {
+  vector<char> s = {'h', 'e', 'l', 'l', 'o'};
+  
+  reverseString(s);
+  
+  return 0;
+}
