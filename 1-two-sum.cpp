0eb358acf15b67412f64f990693c0d2b0e1b81ba
Giovanna Moeller <giovannagmoeller@gmail.com>
2023-06-14 15:19:33 -0300

    two sum

--- 1-two-sum.cpp
+++ 1-two-sum.cpp
@@ -8,6 +8,7 @@ vector<int> twoSum(vector<int>& nums, int target) {
   unordered_map<int, int> hashMap;
   vector<int> res;
   int valueToBeFound = 0;
+  
   for (int i = 0; i < nums.size(); i++) {
     valueToBeFound = target - nums[i];
     if (hashMap.find(valueToBeFound) != hashMap.end()) {
@@ -15,13 +16,13 @@ vector<int> twoSum(vector<int>& nums, int target) {
     }
     hashMap[nums[i]] = i;
   }
+  
   return res;
 }
 
 int main() {
   vector<int> arr = {2, 7, 11, 15};
   vector<int> result = twoSum(arr, 9);
-  
   cout << result[0] << ", " << result[1] << endl;
   return 0;
 }
